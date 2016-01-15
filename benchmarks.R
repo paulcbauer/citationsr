3c903d67fd88e999a93b2999d324db64a3484cfc
Paul <mail@paulcbauer.eu>
2016-01-15 19:48:43 +0100

    Modified benchmarks.R

--- benchmarks.R
+++ benchmarks.R
@@ -2,7 +2,7 @@
 
 
 # Identifcation of citations cases - authorname extraction
-  text <- c("roskedasticity (Beck and Katz, 1995) and year fixed effects ar",
+  citation.markers <- c("roskedasticity (Beck and Katz, 1995) and year fixed effects ar",
   "tries at the same time period will often be correlated (see FOR EXAMPLE Beck and Katz 1995).",
   ". Beck and Katz (1995) identified the pot",
   "ic consistent, Beck and Katz’s (1995) panel corrected",
@@ -36,191 +36,151 @@
 
 
 
-# Identification of running heads - examples
-
-
+# RUNNING HEADS
+  running.heads <- c(
   # Bachner and Hill 2014
-  Policy Studies Journal, 42:S1
-  S52
-  0190-292X © 2014 Policy Studies Organization
-  Published by Wiley Periodicals, Inc., 350 Main Street, Malden, MA 02148, USA, and 9600 Garsington Road, Oxford, OX4 2DQ.
-  Bachner/Hill: Advances in Public Opinion and Policy Attitudes Research
+  "Policy Studies Journal, 42:S1",
+  "S52",
+  "0190-292X © 2014 Policy Studies Organization",
+  "Published by Wiley Periodicals, Inc., 350 Main Street, Malden, MA 02148, USA, and 9600 Garsington Road, Oxford, OX4 2DQ.",
+  "Bachner/Hill: Advances in Public Opinion and Policy Attitudes Research",
 
   # Bisgaard 2015
-  Martin Bisgaard is a PhD candidate in the Department of Political Science at Aarhus University, Denmark.
-  Data and supporting materials necessary to reproduce the numerical results in the article are available in the JOP Dataverse (https://dataverse.harvard
-                                                                                                                                .edu/dataverse/jop) and from the author: http://thedata.harvard.edu/dvn/dv/mbisgaard. An online appendix containing supplemental analyses is available at
-  http://dx.doi.org/10.1086/681591.
-  The Journal of Politics, volume 77, number 3. Published online April 30, 2015. http://dx.doi.org/10.1086/681591
-  q 2015 by the Southern Political Science Association. All rights reserved. 0022-3816/2015/7703-0019$10.00
-  This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM
-  All use subject to JSTOR Terms and Conditions
-  849
-  850 / Bias Will Find a Way Martin Bisgaard
-  This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM
-  All use subject to JSTOR Terms and Conditions
-  Volume 77
-  Number 3
-  July 2015 / 851
-  This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM
-  All use subject to JSTOR Terms and Conditions
-  852 / Bias Will Find a Way Martin Bisgaard
+  "Martin Bisgaard is a PhD candidate in the Department of Political Science at Aarhus University, Denmark.",
+  "Data and supporting materials necessary to reproduce the numerical results in the article are available in the JOP Dataverse (https://dataverse.harvard",
+  "http://dx.doi.org/10.1086/681591.",
+  "The Journal of Politics, volume 77, number 3. Published online April 30, 2015. http://dx.doi.org/10.1086/681591",
+  "q 2015 by the Southern Political Science Association. All rights reserved. 0022-3816/2015/7703-0019$10.00",
+  "This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM",
+  "All use subject to JSTOR Terms and Conditions",
+  "849",
+  "850 / Bias Will Find a Way Martin Bisgaard",
+  "This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM",
+  "All use subject to JSTOR Terms and Conditions",
+  "Volume 77",
+  "Number 3",
+  "July 2015 / 851",
+  "This content downloaded from 23.235.32.0 on Wed, 21 Oct 2015 06:03:23 AM",
+  "All use subject to JSTOR Terms and Conditions",
+  "852 / Bias Will Find a Way Martin Bisgaard",
   # Bolsen et al. 2015
-  DOI: 10.1177/0002716214558393
-  ANNALS, AAPSS, 658, March 2015	271
-  Downloaded from ann.sagepub.com at European Univ Inst - Library on October 21, 2015
-  272
-  THE ANNALS OF THE AMERICAN ACADEMY
-  Downloaded from ann.sagepub.com at European Univ Inst - Library on October 21, 2015
-  Fundamental Beliefs about Global Warming across Groups
-  273
-  274
-  THE ANNALS OF THE AMERICAN ACADEMY
-  Fundamental Beliefs about Global Warming across Groups
+  "DOI: 10.1177/0002716214558393",
+  "ANNALS, AAPSS, 658, March 2015	271",
+  "Downloaded from ann.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "272",
+  "THE ANNALS OF THE AMERICAN ACADEMY",
+  "Downloaded from ann.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "Fundamental Beliefs about Global Warming across Groups",
+  "273",
+  "274",
+  "THE ANNALS OF THE AMERICAN ACADEMY",
+  "Fundamental Beliefs about Global Warming across Groups",
 
   # Cho 2014 - Social Science Quarterly
-  ∗
-  Direct correspondence to Seo-Young Cho, Chair of Empirical Institutional Economics,
-  Faculty of Economics, Philipps University of Marburg, Barf¨ußertor 2, 35037 Marburg, Germany seo.cho@wiwi.uni-marburg.de. The author will share all data and coding for replication
-  purposes.
-  SOCIAL SCIENCE QUARTERLY, Volume 95, Number 3, September 2014
-  
-  C 2013 by the Southwestern Social Science Association
-  DOI: 10.1111/ssqu.12069
-  720
-  Social Science Quarterly
-  International Women’s Convention, Democracy, and Gender Equality
-  721
-  722
-  Social Science Quarterly
-  International Women’s Convention, Democracy, and Gender Equality
-  723
-  724
-  Social Science Quarterly
+  "Direct correspondence to Seo-Young Cho, Chair of Empirical Institutional Economics,",
+  "Faculty of Economics, Philipps University of Marburg, Barf¨ußertor 2, 35037 Marburg, Germany seo.cho@wiwi.uni-marburg.de. The author will share all data and coding for replication",
+  "purposes.",
+  "SOCIAL SCIENCE QUARTERLY, Volume 95, Number 3, September 2014",
+  "C 2013 by the Southwestern Social Science Association",
+  "DOI: 10.1111/ssqu.12069",
+  "720",
+  "Social Science Quarterly",
+  "International Women’s Convention, Democracy, and Gender Equality",
+  "721",
+  "722",
+  "Social Science Quarterly",
+  "International Women’s Convention, Democracy, and Gender Equality",
+  "723",
+  "724",
+  "Social Science Quarterly",
 
   # Cho 2014 - Japanese Journal of Political Science
-  ∗
-
-  This research was supported by Social Science Korea program through the National Research Foundation
-  of Korea (NRF) funded by the Ministry of Education (NRF-2013S1A3A2055205).
-  661
-  662 youngho cho
-  internet and democratic citizenship among the global mass publics 663
-  664 youngho cho
-  internet and democratic citizenship among the global mass publics 665
-  About the author
-  Youngho Cho (Ph.D.) is a lecturer at Hanyang University, Seoul, South Korea. He
-  has several forthcoming articles on public attitudes and knowledge about democracy
-  in the journals Democratization, Political Studies, and Social Indicators Research.
+  "This research was supported by Social Science Korea program through the National Research Foundation",
+  "of Korea (NRF) funded by the Ministry of Education (NRF-2013S1A3A2055205).",
+  "661",
+  "662 youngho cho",
+  "internet and democratic citizenship among the global mass publics 663",
+  "664 youngho cho",
+  "internet and democratic citizenship among the global mass publics 665",
+  "About the author",
+  "Youngho Cho (Ph.D.) is a lecturer at Hanyang University, Seoul, South Korea. He",
+  "has several forthcoming articles on public attitudes and knowledge about democracy",
+  "in the journals Democratization, Political Studies, and Social Indicators Research.",
 
 
   # Clark and Kastellec 2015
-  1Emory
-
-  University, Atlanta, GA, USA
-  University, NJ, USA
-
-  2Princeton
-
-  Corresponding Author:
-    Tom S. Clark, Department of Political Science, Emory University, 327 Tarbutton Hall, 1555
-  Dickey Drive, Atlanta, GA 30322, USA.
-  Email: tom.clark@emory.edu
-  Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015
-  Clark and Kastellec
-  505
-  Keywords
-  Supreme Court, public opinion, source cues, judicial independence
-  Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015
-  506
-  American Politics Research 43(3)
-  Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015
-  Clark and Kastellec
-  507
-  Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015
-  508
-  American Politics Research 43(3)
-  Authors’ Note
-  Complete replication materials can be found on the Dataverse Network at http://
-    dx.doi.org/10.7910/DVN/27712
-
-  Acknowledgment
-  We thank Brandon Bartels, Deborah Beim, Michael Fix, Amy Lerman, Drew Linzer,
-  Neil Mahotra, James Gibson, Jeff Staton, Ali Valenzuela, and Omar Wasow for helpful comments and suggestions.
-
-  Declaration of Conflicting Interests
-  The author(s) declared no potential conflicts of interest with respect to the research,
-  authorship, and/or publication of this article.
-
-  Funding
-  The author(s) received no financial support for the research, authorship, and/or publication of this article.
+  "1Emory",
+  "University, Atlanta, GA, USA",
+  "University, NJ, USA",
+  "2Princeton",
+  "Corresponding Author:",
+    "Tom S. Clark, Department of Political Science, Emory University, 327 Tarbutton Hall, 1555",
+  "Dickey Drive, Atlanta, GA 30322, USA.",
+  "Email: tom.clark@emory.edu",
+  "Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "Clark and Kastellec",
+  "505",
+  "Keywords",
+  "Supreme Court, public opinion, source cues, judicial independence",
+  "Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "506",
+  "American Politics Research 43(3)",
+  "Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "Clark and Kastellec",
+  "507",
+  "Downloaded from apr.sagepub.com at European Univ Inst - Library on October 21, 2015",
+  "508",
+  "American Politics Research 43(3)",
+  "Authors’ Note",
+  "Complete replication materials can be found on the Dataverse Network at http://",
+  "dx.doi.org/10.7910/DVN/27712",
+  "Acknowledgment",
+  "We thank Brandon Bartels, Deborah Beim, Michael Fix, Amy Lerman, Drew Linzer,",
+  "Neil Mahotra, James Gibson, Jeff Staton, Ali Valenzuela, and Omar Wasow for helpful comments and suggestions.",
+  "Declaration of Conflicting Interests",
+  "The author(s) declared no potential conflicts of interest with respect to the research,",
+  "authorship, and/or publication of this article.",
+  "Funding",
+  "The author(s) received no financial support for the research, authorship, and/or publication of this article.",
 
   # Craig et al. 2014
-  Stephen C. Craig is Professor, Department of Political Science, University of Florida. Paulina
-  S. Rippere is Assistant Professor, Division of Social Sciences, Jacksonville University. Marissa Silber
-  Grayson is Assistant Professor, Department of Political Science, Samford University.
-  Address correspondence to Stephen C. Craig, Department of Political Science, University of
-  Florida, P.O. Box 117325, Gainesville, FL 32611-7325, USA. E-mail: sccraig@ufl.edu
-
-  647
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  648
-
-  Stephen C. Craig et al.
-
-  Attack and Response in Political Campaigns
-
-  649
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-
-  650
-
-  Stephen C. Craig et al.
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  Attack and Response in Political Campaigns
-
-  651
-
-  652
-
-  Stephen C. Craig et al.
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  Attack and Response in Political Campaigns
-
-  653
-  Acknowledgments
-  An earlier version of this article was presented at the 2012 annual meetings of the Midwest
-  Political Science Association. We would like to thank Robin West and Roger Austin for
-  their generous assistance in helping us to design these experiments, David Wolfson for
-  donating his professional services in the focus group stage of our project, Richard Nolan
-
-  Attack and Response in Political Campaigns
-
-  669
-
-  and Beth Rosenson for permitting us to recruit their students as subjects, Jim Kitchens
-  for facilitating the Internet survey used in our analysis, and Michael Martinez, Katharine
-  Baldwin, Kenneth Wald, and two anonymous reviewers for their comments on earlier
-  versions.
-
-  Supplemental Material
-
-  Downloaded by [EUI European University Institute] at 03:03 21 October 2015
-
-  Supplemental data for this article can be accessed on the publisher’s Web site at: http://dx.
-  doi.org/10.1080/10584609.2013.879362
+  "Stephen C. Craig is Professor, Department of Political Science, University of Florida. Paulina",
+  "S. Rippere is Assistant Professor, Division of Social Sciences, Jacksonville University. Marissa Silber",
+  "Grayson is Assistant Professor, Department of Political Science, Samford University.",
+  "Address correspondence to Stephen C. Craig, Department of Political Science, University of",
+  "Florida, P.O. Box 117325, Gainesville, FL 32611-7325, USA. E-mail: sccraig@ufl.edu",
+  "647",
+  "Downloaded by [EUI European University Institute] at 03:03 21 October 2015",
+  "648",
+  "Stephen C. Craig et al.",
+  "Attack and Response in Political Campaigns",
+  "649",
+  "650",
+  "Stephen C. Craig et al.",
+  "Downloaded by [EUI European University Institute] at 03:03 21 October 2015",
+  "Attack and Response in Political Campaigns",
+  "651",
+  "652",
+  "Stephen C. Craig e,t al.",
+  "Downloaded by [EUI European University Institute] at 03:03 21 October 2015",
+  "Downloaded by [EUI European University Institute] at 03:03 21 October 2015",
+  "Attack and Response in Political Campaigns",
+  "653",
+  "Acknowledgments",
+  "An earlier version of this article was presented at the 2012 annual meetings of the Midwest",
+  "Political Science Association. We would like to thank Robin West and Roger Austin for",
+  "their generous assistance in helping us to design these experiments, David Wolfson for",
+  "donating his professional services in the focus group stage of our project, Richard Nolan",
+  "Attack and Response in Political Campaigns",
+  "669",
+  "and Beth Rosenson for permitting us to recruit their students as subjects, Jim Kitchens",
+  "for facilitating the Internet survey used in our analysis, and Michael Martinez, Katharine",
+  "Baldwin, Kenneth Wald, and two anonymous reviewers for their comments on earlier",
+  "versions.",
+  "Supplemental Material",
+  "Downloaded by [EUI European University Institute] at 03:03 21 October 2015",
+  "Supplemental data for this article can be accessed on the publisher’s Web site at: http://dx.",
+  "doi.org/10.1080/10584609.2013.879362")
 
 
 
